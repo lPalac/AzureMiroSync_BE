3f5ab96067a92d65cfa45ce022194692b4c1227c
Luka Palac <45354020+lPalac@users.noreply.github.com>
2025-06-17 17:24:25 +0200

    build

--- dist/src/app.js
+++ dist/src/app.js
@@ -39,35 +39,79 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = exports.app = void 0;
 const express_1 = __importStar(require("express"));
 const axios_1 = __importDefault(require("axios"));
+const cors_1 = __importDefault(require("cors"));
 const supabase_js_1 = require("@supabase/supabase-js");
-const getColors_1 = require("./getColors");
+const utils_1 = require("./utils");
 const app = (0, express_1.default)();
 exports.app = app;
 const router = (0, express_1.Router)();
 exports.router = router;
-app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 const supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_URL || "", process.env.SUPABASE_KEY || "");
 router.get("/", (_req, res) => {
     res.status(200).set("Content-Type", "text/plain").send("Hello, World!");
 });
+router.post("/auth", async (req, res) => {
+    const body = (0, utils_1.parseReq)(req);
+    const client_id = body.client_id;
+    const code = body.code;
+    console.log({ code, client_id });
+    const options = {
+        method: "POST",
+        url: "https://api.miro.com/v1/oauth/token",
+        headers: {
+            accept: "application/json",
+            "content-type": "application/x-www-form-urlencoded",
+        },
+        data: {
+            grant_type: "authorization_code",
+            client_id: client_id,
+            client_secret: process.env.MIRO_CLIENT_SECRET,
+            code: code,
+            redirect_uri: process.env.MIRO_REDIRECT_URI,
+        },
+    };
+    axios_1.default
+        .request(options)
+        .then(async (r) => {
+        const { error } = await supabase.from("auth").upsert({
+            userId: r.data.user_id,
+            accessToken: r.data.access_token,
+        }, { onConflict: "userId" });
+        console.log(error);
+        if (error) {
+            res.status(500).end();
+            return;
+        }
+        else {
+            console.log(r.data);
+            return res
+                .status(200)
+                .set("Content-Type", "application/json")
+                .send({ user_id: r.data.user_id, team_id: r.data.team_id });
+        }
+    })
+        .catch(console.log);
+});
 router.post("/", async (req, res) => {
-    let body;
-    // If body is a Buffer
-    if (Buffer.isBuffer(req.body)) {
-        const text = req.body.toString("utf-8");
-        body = JSON.parse(text);
-    }
-    else if (typeof req.body === "string") {
-        body = JSON.parse(req.body);
-    }
-    else {
-        body = req.body;
-    }
+    const body = (0, utils_1.parseReq)(req);
     const PBIId = body.resource?.workItemId;
     const title = body.resource?.revision?.fields?.["System.Title"];
     const status = body.resource?.revision?.fields?.["System.State"];
+    const userId = req.query.userId;
+    const boardId = req.query.boardId;
     console.log({ PBIId, title, status });
     // Get MIROID from supabase
+    const { data: dataAuth, error: errorAuth } = await supabase
+        .from("auth")
+        .select("userId, accessToken")
+        .eq("userId", userId);
+    const accessToken = dataAuth?.[0]?.accessToken;
+    if (!accessToken || errorAuth) {
+        console.log(errorAuth || "User not found!");
+        res.status(200).send("Not Found").end();
+        return;
+    }
     const { data, error } = await supabase
         .from("PBI-mapping")
         .select("miroCardId, created_at, azurePBIId")
@@ -81,10 +125,10 @@ router.post("/", async (req, res) => {
     const getAppCard = async () => {
         const options = {
             method: "GET",
-            url: `https://api.miro.com/v2/boards/${encodeURI(process.env.MIRO_BOARD_ID || "")}/app_cards/${appCardId}`,
+            url: `https://api.miro.com/v2/boards/${encodeURI(boardId || "")}/app_cards/${appCardId}`,
             headers: {
                 accept: "application/json",
-                authorization: `Bearer ${process.env.MIRO_ACCESS_TOKEN}`,
+                authorization: `Bearer ${accessToken}`,
             },
         };
         return await axios_1.default
@@ -100,14 +144,14 @@ router.post("/", async (req, res) => {
         }
         const options = {
             method: "PATCH",
-            url: `https://api.miro.com/v2/boards/${encodeURI(process.env.MIRO_BOARD_ID || "")}/app_cards/${appCardId}`,
+            url: `https://api.miro.com/v2/boards/${encodeURI(boardId || "")}/app_cards/${appCardId}`,
             headers: {
                 accept: "application/json",
                 "content-type": "application/json",
-                authorization: `Bearer ${process.env.MIRO_ACCESS_TOKEN}`,
+                authorization: `Bearer ${accessToken}`,
             },
             style: {
-                cardTheme: (0, getColors_1.getStatusColor)(status),
+                cardTheme: (0, utils_1.getStatusColor)(status),
                 fillBackground: true,
             },
             data: {
@@ -120,8 +164,8 @@ router.post("/", async (req, res) => {
         (options.data.data.fields[0] = {
             value: status,
             iconShape: "square",
-            fillColor: (0, getColors_1.getStatusColor)(status),
-            textColor: "#FFFFFF",
+            fillColor: (0, utils_1.getStatusColor)(status),
+            textColor: "#000000",
             iconUrl: "https://cdn-icons-png.flaticon.com/512/3867/3867669.png",
         }),
             axios_1.default
